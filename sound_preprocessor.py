b78128039c54f48b1b62576a27e9d947367062ea
Nil ☿ <voidpunk.glitch@gmail.com>
2022-06-15 20:17:00 +0200

    changed dataset  (too much bias previously)

--- sound_preprocessor.py
+++ sound_preprocessor.py
@@ -2,10 +2,10 @@ import os
 import kaggle
 
 
-if not os.path.exists('bvc'):
-    os.mkdir('bvc')
 kaggle.api.authenticate()
-dataset = 'ogechukwu/voice'
-print('Downloading and extracting the dataset... ~ 2 min on my system')
-kaggle.api.dataset_download_files(dataset, path='./bvc', unzip=True)
+# dataset = 'ogechukwu/voice'
+dataset = 'tommyngx/fluent-speech-corpus'
+print('Downloading and extracting the dataset... ~ 3 min on my system')
+kaggle.api.dataset_download_files(dataset, path='./', unzip=True)
+os.rename('fluent_speech_commands_dataset', 'fluent_speech')
 
