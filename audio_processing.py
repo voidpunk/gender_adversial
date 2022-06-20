87471dbbe8d19c14daa1922e9ad01c9fa5666cee
Nil ☿ <voidpunk.glitch@gmail.com>
2022-06-20 23:12:12 +0200

    added helper class for image, improved audio class

--- audio_processing.py
+++ audio_processing.py
@@ -4,94 +4,101 @@ import torchaudio
 import numpy as np
 import pandas as pd
 
-
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
-
-def rechannel(aud, new_ch):
-    sig, sr = aud
-    if (sig.shape[0] == new_ch):
-        return sig, sr
-    if (new_ch == 1):
-        resig = sig[:1, :]
-    else:
-        resig = torch.cat([sig, sig])
-    return (resig, sr)
-
-def resample(aud, new_sr):
-    sig, sr = aud
-    if (sr == new_sr):
-        return sig, sr
-    resampled_ch1 = torchaudio.transforms.Resample(sr, new_sr)(sig[:1,:])
-    resampled_ch2 = torchaudio.transforms.Resample(sr, new_sr)(sig[1:,:])
-    resampled_sig = torch.cat([resampled_ch1, resampled_ch2])
-    return (resampled_sig, new_sr)
-
-def resize(aud, max_ms):
-    sig, sr = aud
-    num_ch, sig_len = sig.shape
-    max_len = sr // 1000 * max_ms
-    if sig_len > max_len:
-        sig = sig[:,:max_len]
-    elif sig_len < max_len:
-        pad_begin_len = np.random.randint(0, max_len - sig_len)
-        pad_end_len = max_len - sig_len - pad_begin_len
-        pad_begin = torch.zeros((num_ch, pad_begin_len))
-        pad_end = torch.zeros((num_ch, pad_end_len))
-        sig = torch.cat((pad_begin, sig, pad_end), 1)
-    return (sig, sr)
-
-def time_shift(aud, shift_limit):
-    sig, sr = aud
-    sig_len = sig.shape[1]
-    shift_amt = int(np.random.random() * shift_limit * sig_len)
-    return (sig.roll(shift_amt), sr)
-
-def mel_spectrogram(aud, n_mels=64, n_fft=1024, hop_len=None):
-    sig, sr = aud
-    top_db = 80
-    # shape [channel, n_mels, time]
-    spec = torchaudio.transforms.AmplitudeToDB(top_db=top_db)(sig)
-    spec = torchaudio.transforms.MelSpectrogram(sr, n_fft=n_fft, hop_length=hop_len, n_mels=n_mels)(spec)
-    return spec
-
-def spectro_augment(spec, max_mask_pct=0.1, n_freq_masks=1, n_time_masks=1):
-    _, n_mels, n_steps = spec.shape
-    mask_value = spec.mean()
-    aug_spec = spec
-    freq_mask_param = max_mask_pct * n_mels
-    for _ in range(n_freq_masks):
-        aug_spec = torchaudio.transforms.FrequencyMasking(freq_mask_param)(aug_spec, mask_value)
-    time_mask_param = max_mask_pct * n_steps
-    for _ in range(n_time_masks):
-        aug_spec = torchaudio.transforms.TimeMasking(time_mask_param)(aug_spec, mask_value)
-    return aug_spec
-
-
 class Inference:
 
-    def __init__(self, model, rate, channels, duration, shift_pct):
-        self.model = model
+    def __init__(self, model_path, rate, channels, duration, shift_pct):
+        self.model_path = model_path
         self.rate = rate
         self.channels = channels
         self.duration = duration
         self.shift_pct = shift_pct
+        self.model = torch.load(model_path)
         self.model.eval()
+
+    @staticmethod
+    def _rechannel(aud, new_ch):
+        sig, sr = aud
+        if (sig.shape[0] == new_ch):
+            return sig, sr
+        if (new_ch == 1):
+            resig = sig[:1, :]
+        else:
+            resig = torch.cat([sig, sig])
+        return (resig, sr)
+
+    @staticmethod
+    def _resample(aud, new_sr):
+        sig, sr = aud
+        if (sr == new_sr):
+            return sig, sr
+        resampled_ch1 = torchaudio.transforms.Resample(sr, new_sr)(sig[:1,:])
+        resampled_ch2 = torchaudio.transforms.Resample(sr, new_sr)(sig[1:,:])
+        resampled_sig = torch.cat([resampled_ch1, resampled_ch2])
+        return (resampled_sig, new_sr)
+
+    @staticmethod
+    def _resize(aud, max_ms):
+        sig, sr = aud
+        num_ch, sig_len = sig.shape
+        max_len = sr // 1000 * max_ms
+        if sig_len > max_len:
+            sig = sig[:,:max_len]
+        elif sig_len < max_len:
+            pad_begin_len = np.random.randint(0, max_len - sig_len)
+            pad_end_len = max_len - sig_len - pad_begin_len
+            pad_begin = torch.zeros((num_ch, pad_begin_len))
+            pad_end = torch.zeros((num_ch, pad_end_len))
+            sig = torch.cat((pad_begin, sig, pad_end), 1)
+        return (sig, sr)
+
+    @staticmethod
+    def _time_shift(aud, shift_limit):
+        sig, sr = aud
+        sig_len = sig.shape[1]
+        shift_amt = int(np.random.random() * shift_limit * sig_len)
+        return (sig.roll(shift_amt), sr)
+
+    @staticmethod
+    def _mel_spectrogram(aud, n_mels=64, n_fft=1024, hop_len=None):
+        sig, sr = aud
+        top_db = 80
+        # shape [channel, n_mels, time]
+        spec = torchaudio.transforms.AmplitudeToDB(top_db=top_db)(sig)
+        spec = torchaudio.transforms.MelSpectrogram(
+            sr, n_fft=n_fft, hop_length=hop_len, n_mels=n_mels
+            )(spec)
+        return spec
+
+    @staticmethod    
+    def _spectro_augment(spec, max_mask_pct=0.1, n_freq_masks=1, n_time_masks=1):
+        _, n_mels, n_steps = spec.shape
+        mask_value = spec.mean()
+        aug_spec = spec
+        freq_mask_param = max_mask_pct * n_mels
+        for _ in range(n_freq_masks):
+            aug_spec = torchaudio.transforms.FrequencyMasking(freq_mask_param)(aug_spec, mask_value)
+        time_mask_param = max_mask_pct * n_steps
+        for _ in range(n_time_masks):
+            aug_spec = torchaudio.transforms.TimeMasking(time_mask_param)(aug_spec, mask_value)
+        return aug_spec
     
     def preprocess(self, path):
         raw_aud = torchaudio.load(path)
-        resr_aud = resample(raw_aud, self.rate)
-        rech_aud = rechannel(resr_aud, self.channels)
-        resz_aud = resize(rech_aud, self.duration)
-        shft_aud = time_shift(resz_aud, self.shift_pct)
-        raw_spec = mel_spectrogram(shft_aud, n_mels=64, n_fft=1024, hop_len=None)
-        aug_spec = spectro_augment(raw_spec, max_mask_pct=0.1, n_freq_masks=2, n_time_masks=2)
+        resr_aud = self._resample(raw_aud, self.rate)
+        rech_aud = self._rechannel(resr_aud, self.channels)
+        resz_aud = self._resize(rech_aud, self.duration)
+        shft_aud = self._time_shift(resz_aud, self.shift_pct)
+        raw_spec = self._mel_spectrogram(shft_aud, n_mels=64, n_fft=1024, hop_len=None)
+        aug_spec = self._spectro_augment(raw_spec, max_mask_pct=0.1, n_freq_masks=2, n_time_masks=2)
         return aug_spec
     
     def predict(self, spectrum):
         with torch.no_grad():
-            output = self.model(input.unsqueeze(0).to(device))
-            # print(f'Prediction: {output}')
+            output = self.model(spectrum.unsqueeze(0))
             _, prediction = torch.max(output, 1)
-            # print(f'Prediction: {prediction}')
             return prediction.item()
+    
+    def process_predict(self, path):
+        spectrum = self.preprocess(path)
+        prediction = self.predict(spectrum)
+        return prediction
---
+++ image_processing.py
@@ -0,0 +1,111 @@
+import torch
+import torchvision
+
+
+class Inference:
+
+    def __init__(
+        self, model_name, num_classes, feature_extract,
+        use_pretrained=True, pretrain_path=None
+        ):
+        self.model_name = model_name
+        self.num_classes = num_classes
+        self.feature_extract = feature_extract
+        self.use_pretrained = use_pretrained
+        self.pretrain_path = pretrain_path
+        self.model, self.input_size = self._initialize_model(feature_extract)
+        if self.pretrain_path is not None:
+            self._load_pretrained_model()
+        self.model.eval()
+
+    def _set_parameter_requires_grad(model, feature_extracting):
+        if feature_extracting:
+            for param in model.parameters():
+                param.requires_grad = False
+
+    def _initialize_model(self):
+        model_ft = None
+        input_size = 0
+        if self.model_name == "resnet":
+            """ Resnet18
+            """
+            model_ft = torchvision.models.resnet18(pretrained=self.use_pretrained)
+            self._set_parameter_requires_grad(model_ft, self.feature_extract)
+            num_ftrs = model_ft.fc.in_features
+            model_ft.fc = torch.nn.Linear(num_ftrs, self.num_classes)
+            input_size = 224
+        elif self.model_name == "alexnet":
+            """ Alexnet
+            """
+            model_ft = torchvision.models.alexnet(pretrained=self.use_pretrained)
+            self._set_parameter_requires_grad(model_ft, self.feature_extract)
+            num_ftrs = model_ft.classifier[6].in_features
+            model_ft.classifier[6] = torch.nn.Linear(num_ftrs, self.num_classes)
+            input_size = 224
+        elif self.model_name == "vgg":
+            """ VGG11_bn
+            """
+            model_ft = torchvision.models.vgg11_bn(pretrained=self.use_pretrained)
+            self._set_parameter_requires_grad(model_ft, self.feature_extract)
+            num_ftrs = model_ft.classifier[6].in_features
+            model_ft.classifier[6] = torch.nn.Linear(num_ftrs, self.num_classes)
+            input_size = 224
+        elif self.model_name == "squeezenet":
+            """ Squeezenet
+            """
+            model_ft = torchvision.models.squeezenet1_0(pretrained=self.use_pretrained)
+            self._set_parameter_requires_grad(model_ft, self.feature_extract)
+            model_ft.classifier[1] = torch.nn.Conv2d(
+                512, self.num_classes, kernel_size=(1,1), stride=(1,1)
+                )
+            model_ft.num_classes = self.num_classes
+            input_size = 224
+        elif self.model_name == "densenet":
+            """ Densenet
+            """
+            model_ft = torchvision.models.densenet121(pretrained=self.use_pretrained)
+            self._set_parameter_requires_grad(model_ft, self.feature_extract)
+            num_ftrs = model_ft.classifier.in_features
+            model_ft.classifier = torch.nn.Linear(num_ftrs, self.num_classes)
+            input_size = 224
+        elif self.model_name == "inception":
+            """ Inception v3
+            Be careful, expects (299,299) sized images and has auxiliary output
+            """
+            model_ft = torchvision.models.inception_v3(pretrained=self.use_pretrained)
+            self._set_parameter_requires_grad(model_ft, self.feature_extract)
+            # Handle the auxilary net
+            num_ftrs = model_ft.AuxLogits.fc.in_features
+            model_ft.AuxLogits.fc = torch.nn.Linear(num_ftrs, self.num_classes)
+            # Handle the primary net
+            num_ftrs = model_ft.fc.in_features
+            model_ft.fc = torch.nn.Linear(num_ftrs,self.num_classes)
+            input_size = 299
+        else:
+            print("Invalid model name, exiting...")
+            exit()
+        return model_ft, input_size
+
+    def _load_pretrained_model(self):
+        self.model.load_state_dict(torch.load(self.pretrain_path))
+
+    def preprocess(self, raw_image):
+        transform = torchvision.transforms.Compose([
+            torchvision.transforms.Resize(self.input_size),
+            torchvision.transforms.CenterCrop(self.input_size),
+            torchvision.transforms.ToTensor(),
+            torchvision.transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ])
+        image = transform(raw_image)
+        return torch.unsqueeze(image, 0)
+
+    def predict(self, image):
+        with torch.no_grad():
+            output = self.model(image)
+        prediction = torch.nn.functional.softmax(output, dim=1)[0]
+        return prediction
+
+    def process_predict(self, image_raw):
+        image = self.preprocess(image_raw)
+        prediction = self.predict(image)
+        return prediction
